1d866cb33ba85fe6693b18841b73193056a79d8a
qkrwlgh01 <159628277+qkrwlgh01@users.noreply.github.com>
2024-04-29 11:53:32 +0800

    Update import_words_header.h

--- import_words_header.h
+++ import_words_header.h
@@ -1,9 +1,10 @@
 #ifndef IMPORT_WORDS_HEADER_H
 #define IMPORT_WORDS_HEADER_H
 
-#include <vector>
+#include <vector> // Include for using the vector container from the Standard Template Library (STL)
 #include <string>
 
+// Function declaration: imports words and returns them as a vector of strings
 std::vector<std::string> import_words();
 
 #endif
