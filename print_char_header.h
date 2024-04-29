d4bb6b37630981c2198f6a827ce7dd710cee0408
qkrwlgh01 <159628277+qkrwlgh01@users.noreply.github.com>
2024-04-29 11:54:48 +0800

    Update print_char_header.h

--- print_char_header.h
+++ print_char_header.h
@@ -1,6 +1,7 @@
 #ifndef PRINT_CHAR_HEADER_H
 #define PRINT_CHAR_HEADER_H
 
+// Function declarations for handling character display based on game logic.
 void correct_position(char correct_character);
 void wrong_position(char wrong_character);
 void wrong_character(char wrong_character);
